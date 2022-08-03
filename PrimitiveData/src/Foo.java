7e0a1f565d08a6b2e7fb907ae623cb7c7186268f
Anthony Villegas <villegasanthony87@yahoo.com>
2022-08-03 12:17:13 -0500

    feat: primitive types intro Java 11

---
+++ PrimitiveData/src/Foo.java
@@ -0,0 +1,15 @@
+public class Foo {
+
+    public static void main(String[] args) {
+
+        // Declaration of local variables, local to the main method
+        boolean myBoolean;
+        byte myByte;
+        char myFirstChar, mySecondChar;
+        short myShort;
+        int myInt;
+        long myLong;
+        float myFloat;
+        double myFirstDouble, mySecondDouble;
+    }
+}
