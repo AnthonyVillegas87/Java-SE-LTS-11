18adb2d749a52ba2b07fcf830ac1b87e4226d7c3
Anthony Villegas <villegasanthony87@yahoo.com>
2022-08-04 00:08:55 -0500

    feat: Strings recap

--- LVTI/src/VarLoops.java
+++ LVTI/src/VarLoops.java
@@ -1,7 +1,19 @@
 public class VarLoops {
 
-
+    static float tax_rate = 0.06F;
     public static void main(String[] args) {
+
+
+        var result = 0;
+        var tax_rate = 0.05;    // Line 1
+        var price = 100;
+        var adjusted_tax = price * tax_rate;  // Line 2
+    //    result = price + adjusted_tax;  // Line 3
+        System.out.println("Adjusted price = " + result);
+
+
+
+
         // use LVTI var in a regular for loop
         for(var i = 0; i < 10; i++) {
             System.out.println("i was inferred to be an int, value = " + i);
